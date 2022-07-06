bd15f0c479088ae9d02835f772fd4d659961a2f7
teeratchapat <teeratchapat.c@kkumail.com>
2022-07-07 01:03:54 +0700

    test: sent data branch to main

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 function getCurrentTime() {
     const date_now = new Date();
     const hours = date_now.getHours().toString().padStart(2, "0");
@@ -12,3 +13,21 @@ function changeTimeToBinary(timeString) {
 }
 
 console.log(changeTimeToBinary(getCurrentTime()));
+=======
+function currentTime(){
+    const date_now = new Date();
+    const hours = date_now.getHours().toString().padStart(2, '0');
+    const minutes = date_now.getMinutes().toString().padStart(2, '0');
+    const seconds = date_now.getSeconds().toString().padStart(2, '0');
+    // console.log(`${hours}${minutes}${seconds}`);
+    return `${hours}${minutes}${seconds}`;
+}
+
+function changesTimeToBinary(timeString){
+    return timeString.split('').map((time) => parseInt(time, 10).toString(2).padStart(4, '0'));
+}
+
+changesTimeToBinary(currentTime())
+console.log(changesTimeToBinary(currentTime()));
+  
+>>>>>>> Stashed changes
