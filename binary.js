3fe42de3891fd80166e6e837c4a0c888892f6912
teeratchapat <teeratchapat.c@kkumail.com>
2022-07-09 12:24:38 +0700

    feat(controller): add binaryclock and show on website

--- binary.js
+++ binary.js
@@ -1,32 +1,27 @@
 function getCurrentTime() {
-    const date_now = new Date();
-    const hours = date_now.getHours().toString().padStart(2, "0");
-    const minutes = date_now.getMinutes().toString().padStart(2, "0");
-    const seconds = date_now.getSeconds().toString().padStart(2, "0");
+    const dateNow = new Date();
+    const hours = dateNow.getHours().toString().padStart(2, "0");
+    const minutes = dateNow.getMinutes().toString().padStart(2, "0");
+    const seconds = dateNow.getSeconds().toString().padStart(2, "0");
     return `${hours}${minutes}${seconds}`;
 }
 
 function changeTimeToBinary(timeString) {
-    binaryArr =  timeString.split("")
-    return binaryArr.map((num) => parseInt(num, 10).toString(2).padStart(4, '0'));;
+    const binaryArr = timeString.split("").map((time) => parseInt(time, 10).toString(2).padStart(4, "0"));
+    return binaryArr;
 }
-
-// console.log(changeTimeToBinary(getCurrentTime()));
 
 function actionBinaryView(){
     const binaryTime = changeTimeToBinary(getCurrentTime());
-    for(let i=0;i<binaryTime.length;i++){
+    for(let i=0;i<6;i++){
         const temp = binaryTime[i].split("");
         for(let j=0;j<4;j++){
-            let elem = document.getElementById("col"+i+"row2"+j);
-            // console.log("i = "+i+"j = "+j);
-            elem.classList.add("work-circle");
-            elem.classList.add("work-circle");
-            // if (temp == 1){
-            //     elem.classList.add("work-circle");
-            // }else{
-            //     elem.classList.remove("work-circle");
-            // }
+            let elem = document.getElementById(`col${i+1}row${j+1}`);
+            if (temp[j] == 1){
+                elem.classList.add("work-circle");
+            }else{
+                elem.classList.remove("work-circle");
+            }
         }
     }
    
